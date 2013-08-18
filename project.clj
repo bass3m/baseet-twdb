5b2e014ae791d4caafff3e3344361fcfdfbfe669
Bassem Youssef <bassem@outlook.com>
2013-08-18 09:43:24 -0700

    update version and dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject baseet-twdb "0.1.1-SNAPSHOT"
+(defproject baseet-twdb "0.1.2-SNAPSHOT"
   :description "Simple library for accessing baseet tweets db"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/bass3m/baseet-twdb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :profiles {:dev  {:plugins  [[lein-ancient "0.4.4"]]}}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.ashafa/clutch "0.4.0-RC1"]
-                 [suweet "0.1.4-SNAPSHOT"]])
+                 [suweet "0.1.6-SNAPSHOT"]])
